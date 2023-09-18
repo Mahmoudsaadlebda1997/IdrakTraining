47d55e76886b86efbee23c8adc56097ee967b92b
Mahmoudsaadlebda1997 <mahmodsaadlebda@gmail.com>
2023-09-18 16:43:10 +0300

    adding Project laravel

---
+++ resources/views/contactUs.blade.php
@@ -0,0 +1,106 @@
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>ContactPage</title>
+</head>
+
+<body>
+<div class="container my-3 my-sm-5">
+
+
+    <h1 class="mb-sm-4 text-center mb-4">Contact Us</h1>
+    <hr>
+    <div class="row">
+        <div class="col-12 col-md-6">
+
+
+
+            <h4>Address</h4>
+            <address>
+                <strong>
+                    Plastine City
+                </strong>
+                <br>
+                #41 Ghaza Street <br>
+                Ghaza,Plastine <br>
+                <abbr title="Telephone">T:</abbr> <a href="tel:+919036242596">+91 90362 42596</a> <br>
+                <abbr title="Mail">M:</abbr><a href="mailto:idrakCompany@website.com">idrakCompany@website.com</a><br>
+            </address>
+            <p>
+             Social Links :
+            <a href="https://www.edraak.org/"> Website</a>
+            <a href="https://www.facebook.com/idrakfinefinishing/"> Facebook</a>
+            <a href="https://business.linkedin.com/marketing-solutions/success"> LinkedIn</a>
+            </p>
+        </div>
+        <div class="col-12 col-md-6">
+
+
+            <h4>Course hours</h4>
+            <p>
+                Monday: <span class="float-right">12 am - 2 pm  </span> <br>
+            </p>
+            <p>
+                Wedenesday: <span class="float-right"> 12 am - 2 pm</span> <br>
+            </p>
+        </div>
+    </div>
+    <div class="row">
+
+        <div class="col-12 col-md-6 mb-3">
+            <h4>Send us a message</h4>
+            <form action="#">
+                <div class="row">
+                    <div class="col-12 col-md-6">
+
+                        <div class="form-group">
+                            <label for="name">Name</label>
+                            <input type="text" id="name" class="form-control" placeholder="name" required>
+
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-6">
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <input type="email" id="email" class="form-control" placeholder="example@domain.com" required>
+
+                        </div>
+                    </div>
+
+                </div>
+                <div class="form-group">
+                    <label for="textarea">Message:</label>
+                    <textarea class="form-control" placeholder="Write your message here..." name="" id="textarea" cols="30" rows="10" required></textarea>
+
+
+
+                </div>
+                <button type="submit" class="btn btn-outline-primary">Submit</button>
+            </form>
+        </div>
+        <div class="col-12 col-md-6">
+
+
+            <h4>Where to find us</h4>
+            <div class="embed-responsive embed-responsive-4by3">
+                <iframe class="embed-responsive-item" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3022.6175453420647!2d-73.98784413479707!3d40.748440379327945!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c259a9aeb1c6b5%3A0x35b1cfbc89a6097f!2sEmpire+State+Building%2C+350+5th+Ave%2C+New+York%2C+NY+10118%2C+USA!5e0!3m2!1sda!2sdk!4v1491841211721"
+                        width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+</script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+</script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+</script>
+</body>
+
---
+++ resources/views/home.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Idrak Company</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+    <!-- Styles -->
+    <style>
+        .col1{
+            background: #1b48ff;
+            height:25vh;
+        }
+        .col2{
+            height:100vh;
+            overflow-y: scroll;
+            padding: 4rem;
+        }
+
+        .content-bar {
+            height: auto;
+            padding: 20px;
+        }
+
+        .content-bar .logo-box {
+            max-width: 350px;
+            height: auto;
+            padding: 10px;
+        }
+    </style>
+</head>
+<body>
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-sm-4 col1 d-none d-sm-block">
+            <img src="https://s3-sa-east-1.amazonaws.com/cdn.eskive.com/templates/Eskive/one.svg" alt="Background" class="col-4 p-0 fixed-top min-vh-100" style="object-fit: cover; object-position: right top;">
+        </div>
+        <div class="col-sm-8 col-xs-12 col2">
+    <h1 style="text-align: center; font-size: 70px; color: midnightblue">Welcome To Our Website</h1>
+            <div class="content-bar"><!----><div class="logo-box"><img src="https://img0cf.b8cdn.com/images/logo/76/2051676_logo_1614971313_n.png" alt="Logo" class="logo-box img-responsive"></div>
+
+                <p style="color: darkblue">  We Are Idrak Company We Here To Provide u With Sollutions For Ur Problems We Make Mobile Applications + Website Application  Contact Us</p>
+
+                <a href="{{ route('getContactUs') }}"  class="text-center font-semibold" style="color: darkblue; font-size: larger"> Contact Us</a>
+
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
